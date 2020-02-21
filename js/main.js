dcc95b6c2fb702164b5c41983f2afb5aaf4a7f4d
Vlad Starshinov <54811872+vladistarshinov@users.noreply.github.com>
2020-02-21 10:19:57 +0300

    Add render text content

--- js/main.js
+++ js/main.js
@@ -14,6 +14,9 @@ const DATA = {
     deadlinePercent: [20, 17, 15]
 };
 
+const DAY_STRING = ['день', 'дня', 'дней'];
+const ANSWER_STRING = ['Нет', 'Да'];
+
 const startButton = document.querySelector('.start-button'),
       firstScreen = document.querySelector('.first-screen'),
       mainForm = document.querySelector('.main-form'),
@@ -21,10 +24,24 @@ const startButton = document.querySelector('.start-button'),
       endButton = document.querySelector('.end-button'),
       total = document.querySelector('.total'),
       fastRange = document.querySelector('.fast-range'),
-      totalPriceSum = document.querySelector('.total_price__sum');
+      totalPriceSum = document.querySelector('.total_price__sum'),
+      typeSite = document.querySelector('.type-site'),
+      maxDeadline = document.querySelector('.max-deadline'),
+      rangeDeadline = document.querySelector('.range-deadline'),
+      deadlineValue = document.querySelector('.deadline-value'),
+      desktopTemplatesValue = document.querySelector('.desktopTemplates_value'),
+      adaptValue = document.querySelector('.adapt_value'),
+      mobileTemplatesValue = document.querySelector('.mobileTemplates_value'),
+      editableValue = document.querySelector('.editable_value');
 
 const adaptCheckbox = document.getElementById('adapt'),
       mobileTemplatesCheckbox = document.getElementById('mobileTemplates');
+
+// возвращает число и слово
+function declOfNum(n, titles) {
+    return n + ' ' + titles[n % 10 === 1 && n % 100 !== 11 ?
+        0 : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2];
+}
 
 console.dir(startButton);
 
@@ -44,10 +61,27 @@ function hideElem(elem) {
     elem.style.display = 'none';
 }
 
+function renderTextContent(total, site, minDay, maxDay) {
+    totalPriceSum.textContent = total;
+    typeSite.textContent = site;
+    rangeDeadline.min = minDay;
+    rangeDeadline.max = maxDay;
+    maxDeadline.textContent = declOfNum(maxDay, DAY_STRING);
+    deadlineValue.textContent = declOfNum(rangeDeadline.value, DAY_STRING);
+
+    desktopTemplatesValue.textContent = desktopTemplates.checked ? ANSWER_STRING[1] : ANSWER_STRING[0];
+    adaptValue.textContent = adapt.checked ? ANSWER_STRING[1] : ANSWER_STRING[0];
+    mobileTemplatesValue.textContent = mobileTemplates.checked ? ANSWER_STRING[1] : ANSWER_STRING[0];
+    editableValue.textContent= editable.checked ? ANSWER_STRING[1] : ANSWER_STRING[0]; 
+}
+
 function priceCalculation(elem) {
     let result = 0,
         index = 0,
-        options = [];
+        options = [],
+        site = '',
+        minDeadlineDay = DATA.deadlineDay[index][0],
+        maxDeadlineDay = DATA.deadlineDay[index][1];
 
     if (elem.name === 'whichSite') {
         for (const item of formCalculate.elements) {
@@ -60,11 +94,15 @@ function priceCalculation(elem) {
     for (const item of formCalculate.elements) {
         if (item.name === 'whichSite' && item.checked) {
             index = DATA.whichSite.indexOf(item.value);
+            site = item.dataset.site;
+            minDeadlineDay = DATA.deadlineDay[index][0];
+            maxDeadlineDay = DATA.deadlineDay[index][1];
         }
         else if (item.classList.contains('calc-handler') && item.checked) {
            options.push(item.value);
         }
     }
+
     options.forEach(function(key) {
         if (typeof(DATA[key]) === 'number') {
             if (key === 'sendOrder') {
@@ -82,7 +120,8 @@ function priceCalculation(elem) {
     })
 
     result += DATA.price[index];
-    totalPriceSum.textContent = result;
+    renderTextContent(result, site, minDeadlineDay, maxDeadlineDay);
+
 }
 
 function handlerCallBackForm(event) {
